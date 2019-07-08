1eaa6bb41117e89d8e640e2e248bea8c6ab299e7
thomasdarmawan <thomas.darmawan9@gmail.com>
2019-07-08 10:27:39 +0700

    asd

--- core/aurora-crud/aura/sysaura.js
+++ core/aurora-crud/aura/sysaura.js
@@ -1,20 +1,15 @@
 //get connection function from file compile.js with name aurora_enviroment//
-var express = require('./compile.js.js');
+var express = require('../../compile.js');
 //get express module//
 var express = require('express');
 var app     = express();
 //declare var con from enviroment//
-var con = enviroment();
 
 //function insert//
-function insert{
-    enviroment.query();
+function insert(){
+    var data = [{id:1, name: "one"},{id: 2, name: "two"}];
+    
+    return data;
 }
 
-//function update//
-function update{
-    enviroment.query();
-}
-
-module.exports.insert = insert;
-module.exports.update = update;
+module.exports = insert;
--- route/api.js
+++ route/api.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app     = express();
-var port    =   process.env.PORT || 8080;
+var port    = process.env.PORT || 8080;
+var path    = require('path');
+var sysaura  = require('../core/aurora-crud/aura/sysaura');
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 app.get('/myfile', function(req, res) {
-    res.send('this is a sample!');  
+    res.send(sysaura.insert());
+    // console.log(res);
 });
 
 app.listen(port);
