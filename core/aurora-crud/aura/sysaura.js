784645ba3256297e66f6ad0f939ac155d081d33f
thomasdarmawan <thomas.darmawan9@gmail.com>
2019-07-09 10:19:07 +0700

    asd

--- core/aurora-crud/aura/sysaura.js
+++ core/aurora-crud/aura/sysaura.js
@@ -6,16 +6,36 @@ var app     = express();
 var table = "";
 
 //declare var con from enviroment//
+var enviroment = require('../../compile.js');
+
+
+function main(){
+
+    module.exports.insert = insert;
+    module.exports.models = models;
+
+}
 
 //function insert//
 function insert(){
+    //contoh : enviroment.query(){
+    //              if(err){
+    //              "error cuy"
+    //              };
+    //         }
+
+
+    //dibawah ini coba2
     var data = [{id:1, name: "one"},{id: 2, name: "two"}];
     return data;
+    //
+
+
 }
 
 function models(val){
+    //sementara function model kosong dulu
     table = val;
 }
 
-module.exports.insert = insert;
-module.exports.models = models;
+module.exports.main = main;
--- core/aurora-crud/query/mysql.js
+++ core/aurora-crud/query/mysql.js
@@ -1,4 +1,11 @@
-//insert data//
-var insert = {
-    
+//get connection function from file compile.js with name aurora_enviroment//
+var express = require('../../compile.js');
+//get express module//
+var express = require('express');
+var app     = express();
+
+function query(){
+        // bingung nih problem harus tulis di clickup
 }
+
+module.exports.query = query;
