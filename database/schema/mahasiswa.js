4c8173b659e860ab61de6b5bdb759a6119fbcc43
CalvinJO <cjob280@gmail.com>
2019-07-12 21:04:57 +0700

    add unique 2

--- database/schema/mahasiswa.js
+++ database/schema/mahasiswa.js
@@ -2,7 +2,7 @@ module.exports.up = {
     'table_name' : 'mahasiswa',
     'blueprint' : function(){
         increment('id');
-        varchar('nama', 100),nullable();
+        varchar('nama', 100),unique();
         bigInteger('NIK'),nullable();
         index('nik_index',['NIK']);
     }
