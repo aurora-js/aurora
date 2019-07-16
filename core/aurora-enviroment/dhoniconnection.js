037065beda7ea5fcd2ff2681e3d714f9aecbbb37
dhoni1717 <mochamadramadhoni0@gmail.com>
2019-07-16 14:15:47 +0700

    dhoni

--- core/aurora-enviroment/dhoniconnection.js
+++ core/aurora-enviroment/dhoniconnection.js
@@ -24,48 +24,59 @@ db.connect(function (err) {
 
         //create database
 
-        var sql = "CREATE TABLE coba (nim INT(10) PRIMARY KEY, nama VARCHAR(50))";
-        db.query(sql, function (err, result) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Table created");
-            }
-        })
+        // var sql = "CREATE TABLE coba (nim INT(10) PRIMARY KEY, nama VARCHAR(50))";
+        // db.query(sql, function (err, result) {
+        //     if (err) {
+        //         console.log(err);
+        //     } else {
+        //         console.log("Table created");
+        //     }
+        // })
     }
 });
 
 //delete table
-var sql = "DROP TABLE aurora_table";
-db.query(sql, function (err, result) {
-    if (err) {
-        console.log("error delete table!");
-    } else {
-        console.log("delete success!");
-    }
-})
+// var sql = "DROP TABLE aurora_table";
+// db.query(sql, function (err, result) {
+//     if (err) {
+//         console.log("error delete table!");
+//     } else {
+//         console.log("delete success!");
+//     }
+// })
 
 //update database
-db.query("SELECT * ALTER table FROM coba add code", function (err, result, fields) {
-    if (err) {
-        console.log("error get column table database");
-    } else {
-        console.log("update column database success")
-    }
-});
+//db.query("SELECT * ALTER table FROM coba add code", function (err, result, fields) {
+//  if (err) {
+//    console.log("error get column table database");
+// } else {
+//   console.log("update column database success")
+// }
+//});
 
 //function node js mysql
-db.query("SELECT * FROM coba", function (err, result, fields) {
-    if (err) {
-        console.log("error get function");
-    } else {
-        console.log("success get function")
-    }
-});
+// db.query("SELECT * FROM coba", function (err, result, fields) {
+//     if (err) {
+//         console.log("error get function");
+//     } else {
+//         console.log("success get function")
+//     }
+// });
 
+//function create table
+function create_table(name) {
+    var sql = "CREATE TABLE " + name + " (nim INT(10) PRIMARY KEY, nama VARCHAR(50))";
+    console.log(sql);
+    db.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("success create table")
+        }
+    })
+};
 
-
-
+create_table('dhoni');
 
 
 
