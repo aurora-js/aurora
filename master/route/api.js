1fc2475b87f9d91784a976860a7adc8ff5e2fe28
CalvinJO <cjob280@gmail.com>
2019-09-17 14:28:38 +0700

    Remove dummy file

--- master/route/api.js
+++ master/route/api.js
@@ -11,31 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'../views'));
-
-
-
-
-
-app.get('/members', function(req, res) {
-	require('../controllers/membersController').index(req,res);
-});
-
-app.post('/members', function(req, res) {
-	res.send(require('../controllers/membersController').create(req,res));
-});
-
-app.put('/members/:id', function(req, res) {
-	res.send(require('../controllers/membersController').update(req,res));
-});
-
-app.get('/show/edit/members/:id', function(req, res) {
-	require('../controllers/membersController').show_edit(req,res);
-});
-
-app.delete('/members/:id', function(req, res) {
-	res.send(require('../controllers/membersController').erase(req,res));
-});
-
 app.listen(port);
 
 console.log('Aurora Serve on port ' + port);
