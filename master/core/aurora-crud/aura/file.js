366794d0428d0577b1fdff6b1e6d11b53bd961b9
thomasdarmawan <thomas.darmawan9@gmail.com>
2019-12-30 22:03:59 +0700

    asd

--- master/core/aurora-crud/aura/file.js
+++ master/core/aurora-crud/aura/file.js
@@ -513,10 +513,10 @@ function generate_route(name,route,model){
             syntax_route = syntax_route+"app.get('/"+route+"', function(req, res) {\n\trequire('../controllers/"+name+"').index(req,res);\n});";
 
             //For create syntax
-            syntax_route = syntax_route+"\n\napp.post('/"+route+"', function(req, res) {\n\tres.send(require('../controllers/"+name+"').create(req,res));\n});";
+            syntax_route = syntax_route+"\n\napp.post('/"+route+"', function(req, res) {\n\trequire('../controllers/"+name+"').create(req,res);\n});";
 
             //For update syntax
-            syntax_route = syntax_route+"\n\napp.put('/"+route+"/:"+obj_get_field[0]+"', function(req, res) {\n\tres.send(require('../controllers/"+name+"').update(req,res));\n});";
+            syntax_route = syntax_route+"\n\napp.put('/"+route+"/:"+obj_get_field[0]+"', function(req, res) {\n\trequire('../controllers/"+name+"').update(req,res);\n});";
 
             //For show update syntax
             syntax_route = syntax_route+"\n\napp.get('/show/edit/"+route+"/:"+obj_get_field[0]+"', function(req, res) {\n\trequire('../controllers/"+name+"').show_edit(req,res);\n});";
